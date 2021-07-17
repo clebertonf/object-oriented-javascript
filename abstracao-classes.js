73b957c9050f93881d8766fdafda0d69129976e9
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-17 12:04:28 -0300

    Classe produto

---
+++ abstracao-classes.js
@@ -0,0 +1,33 @@
+// São classes diversas, praticando os conceitos de Abstração e contrução de classes em si.
+// Exemplos pequenos somente para praticar
+
+class Product {
+    constructor(){
+        this.name = '';
+        this.price = '';
+    }
+
+    sell(name, price){
+        this.name = name;
+        this.price = price;
+
+        console.log(
+        `
+        ==== Ǹota Fiscal === 
+          Produto: ${this.name}
+          Preço: ${this.price}
+        =====================
+        `);
+    }
+
+    cancel(name){
+        this.name = name;
+        console.log(`Produto ${this.name} cancelado!`)
+    }
+
+}
+
+const Product1 = new Product();
+
+Product1.sell('Arroz', 15.99);
+Product1.cancel('Arroz');

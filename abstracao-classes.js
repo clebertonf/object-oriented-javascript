11ff5e5ac6e5e17f4a31ac0a7c9c663374e6d698
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-17 12:43:21 -0300

    Descomentando linhas

--- abstracao-classes.js
+++ abstracao-classes.js
@@ -2,75 +2,75 @@
 // Exemplos pequenos somente para praticar
 
 // Produto de uma loja
-// class Product {
-//     constructor() {
-//         this.name = '';
-//         this.price = '';
-//     }
+class Product {
+    constructor() {
+        this.name = '';
+        this.price = '';
+    }
 
-//     sell(name, price) {
-//         this.name = name;
-//         this.price = price;
+    sell(name, price) {
+        this.name = name;
+        this.price = price;
 
-//         console.log(
-//             `
-//         ==== Ǹota Fiscal === 
-//           Produto: ${this.name}
-//           Preço: ${this.price}
-//         =====================
-//         `);
-//     }
+        console.log(
+            `
+        ==== Ǹota Fiscal === 
+          Produto: ${this.name}
+          Preço: ${this.price}
+        =====================
+        `);
+    }
 
-//     cancel(name) {
-//         this.name = name;
-//         console.log(`Produto ${this.name} cancelado!`)
-//     }
+    cancel(name) {
+        this.name = name;
+        console.log(`Produto ${this.name} cancelado!`)
+    }
 
-// }
+}
 
-// const Product1 = new Product();
+const Product1 = new Product();
 
-// Product1.sell('Arroz', 15.99);
-// Product1.cancel('Arroz');
+Product1.sell('Arroz', 15.99);
+Product1.cancel('Arroz');
 
 // carrinho de compras ecommerce
 
-// class ShoppingCart {
-//     constructor() {
-//         this.ProductName = '';
-//         this.listProducts = [];
-//         this.price = 0;
-//         this.totalPrice = 0;
-//     }
+class ShoppingCart {
+    constructor() {
+        this.ProductName = '';
+        this.listProducts = [];
+        this.price = 0;
+        this.totalPrice = 0;
+    }
 
-//     pickProducts(name, price) {
+    pickProducts(name, price) {
 
-//         const Product = {
-//            ProductName: this.ProductName = name,
-//            ProductPrice: this.price = price,
-//         }
+        const Product = {
+           ProductName: this.ProductName = name,
+           ProductPrice: this.price = price,
+        }
 
 
-//         this.listProducts.push(Product);
+        this.listProducts.push(Product);
 
-//         return this.listProducts;
-//     };
+        return this.listProducts;
+    };
 
-//     sumProducts() { 
-//        const total = this.listProducts.reduce((acc, curr) => acc + curr.ProductPrice, 0);
-//        return {
-//            total,
-//        }
-//     }
-// }
+    sumProducts() { 
+       const total = this.listProducts.reduce((acc, curr) => acc + curr.ProductPrice, 0);
+       return {
+           total,
+       }
+    }
+}
 
-// const ProductCart1 = new ShoppingCart();
+const ProductCart1 = new ShoppingCart();
 
-// console.log(ProductCart1.pickProducts('TV', 5000));
-// console.log(ProductCart1.pickProducts('SOM', 4000));
-// console.log(ProductCart1.pickProducts('Celular', 2000));
+console.log(ProductCart1.pickProducts('TV', 5000));
+console.log(ProductCart1.pickProducts('SOM', 4000));
+console.log(ProductCart1.pickProducts('Celular', 2000));
 
-// console.log(ProductCart1.sumProducts());
+console.log(ProductCart1.sumProducts());
 
 // Aluguel de carros
 
